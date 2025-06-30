ef9fa44fc5a220ea08a4c1df93f3d1464576caab
HisyamIkhwana <88410078+HisyamIkhwana@users.noreply.github.com>
2025-06-30 09:50:18 +0700

    Update 2025_06_24_085014_create_sesis_table.php

--- database/migrations/2025_06_24_085014_create_sesis_table.php
+++ database/migrations/2025_06_24_085014_create_sesis_table.php
@@ -6,26 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('sesis', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('komputer_id'); // Relasi ke Komputer
+            $table->unsignedBigInteger('komputer_id'); 
             $table->dateTime('waktu_mulai');
-            $table->dateTime('waktu_selesai')->nullable(); // Nullable karena sesi bisa aktif
-            $table->integer('durasi')->nullable(); // Durasi dalam menit
+            $table->dateTime('waktu_selesai')->nullable(); 
+            $table->integer('durasi')->nullable(); 
             $table->timestamps();
 
             $table->foreign('komputer_id')->references('id')->on('komputers')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('sesis');
